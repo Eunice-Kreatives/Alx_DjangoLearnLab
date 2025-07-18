15114507a2b50a9ed4e6f92d3ef982fb250e89eb
Eunice Akpa <akpaeunice2242@gmail.com>
2025-07-18 17:45:57 +0100

    Implemented custom permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,7 +19,12 @@ urlpatterns = [
     path('member-role/', views.member_view, name='member_view'),
 
 
-    path('books/add/', add_book, name='add_book'),
-    path('books/<int:book_id>/edit/', edit_book, name='edit_book'),
-    path('books/<int:book_id>/delete/', delete_book, name='delete_book'),
-]
+    path('books/add/', views.add_book, name='add_book'),          
+    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
+
+
+# relationship_app/urls.py
+
+
+    
