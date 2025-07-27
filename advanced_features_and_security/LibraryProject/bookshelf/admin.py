51799e8803b4773c47bf8e0b22067b920c6f22d4
Eunice Akpa <akpaeunice2242@gmail.com>
2025-07-27 15:37:04 +0100

    Update advanced features and security

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -3,25 +3,21 @@ from .models import Book
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from django.utils.translation import gettext_lazy as _
+             
 
-
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year') 
-    search_fields = ('title', 'author')                     
-    list_filter = ('publication_year',)    
-
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_superuser')
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
     fieldsets = UserAdmin.fieldsets + (
-        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo')
+        }),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo')
+        }),
     )
 
-              
-
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
