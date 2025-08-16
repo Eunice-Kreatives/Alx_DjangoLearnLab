6229255c939fc296ccf4bda9ab5b301c35b944eb
Eunice Akpa <akpaeunice2242@gmail.com>
2025-08-16 12:50:47 +0100

    Implemented Tagging and Searching functionalities

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Profile, Comment
+from taggit.forms import TagWidget
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -26,6 +27,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
+
+        
 
 class CommentForm(forms.ModelForm):
     content = forms.CharField(
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,20 +3,11 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from taggit.managers import TaggableManager
 # Create your models here.
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('blog:tag-detail', kwargs={'pk': self.pk})
     
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    tags = TaggableManager
+    tags = TaggableManager()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'posts')
 
